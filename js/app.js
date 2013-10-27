36bcbc7fd87ff496935e7d06523ac30fa092b98f
Douglas <hefesoft@hotmail.com>
2013-10-27 17:50:37 -0500

    app

--- js/app.js
+++ js/app.js
@@ -6,17 +6,24 @@ var eventsApp = angular.module('eventsApp',['ngCookies'])
             {
                 templateUrl: '/templates/EventList.html',
                 controller: 'EventListController'
-            });  
-                
+            });
         $routeProvider.when('/acciones',
             {
                 templateUrl: '/templates/Acciones.html',
                 controller: 'accionesController'
             });
 
-        $routeProvider.otherwise({redirectTo: '/'});
+        $routeProvider.otherwise({redirectTo: '/dashboard'});
         $locationProvider.html5Mode(true);
     });
+
+eventsApp.run(function ($rootScope) {
+    $rootScope.azureURL  = 'https://indicadores.azure-mobile.net/';
+    $rootScope.azureAppKey   = 'oiXKUUWOqgqBiVrJlHShkWtAgjsCsc35';
+});
+
+
+
 
 eventsApp.run(function ($rootScope) {
     $rootScope.azureURL  = 'https://indicadores.azure-mobile.net/';
